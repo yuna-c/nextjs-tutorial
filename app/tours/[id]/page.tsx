da4660b738cbf6696c3ebd93fde5add31343221a
yuna <yuna744@gmail.com>
2025-02-12 19:51:16 +0900

    remote image - 외부 도메인 next.config.js 설정(보안)

--- app/tours/[id]/page.tsx
+++ app/tours/[id]/page.tsx
@@ -1,5 +1,6 @@
 import mapsImg from '@/images/image.jpeg'
 import Image from 'next/image'
+const url = 'https://www.course-api.com/images/tours/tour-1.jpeg'
 
 function page({ params }: { params: { id: string } }) {
   return (
@@ -10,7 +11,10 @@ function page({ params }: { params: { id: string } }) {
           local image
           원본 이미지 크기이기 때문에 용량이 커짐, 캐싱 느림
           w/h 추가시 용량 최적화 가능
-          Largest Contentful Paint (LCP) 최적화 권상 사항 뜸 = priority 
+          Largest Contentful Paint (LCP) 최적화 권상 사항 뜸 = priority (preload처리)
+          Next.js의 자동 이미지 최적화를 비활성화 = unoptimized
+          빠른 페이지 로딩 기본 적용 (뷰포트 진입 시 로드됨, 옵션으로 블러(blur-up) 플레이스홀더) = lazy loading
+          원격 서버의 이미지도 필요에 따라 실시간으로 크기 조정 가능
         */}
         <div>
           <Image src={mapsImg} alt="maps" priority width={192} height={192} className="w-48 h-48 object-cover rounded"></Image>
@@ -18,9 +22,12 @@ function page({ params }: { params: { id: string } }) {
         </div>
         {/* 
           remote image 
-          
+          기본 보안 설정으로 인해 **외부 이미지(호스트가 다른 URL)**를 사용할 경우, next.config.js에서 해당 도메인을 허용
         */}
-        <div></div>
+        <div>
+          <Image src={url} alt="tour" width={192} height={192} priority className="w-48 h-48 object-cover rounded"></Image>
+          <h2>remote Image</h2>
+        </div>
       </section>
     </div>
   )
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+// domains vs remotePatterns
+const nextConfig = {
+  // images: {
+  //   domains: ["www.course-api.com"], // ✅ 특정 도메인의 모든 이미지 허용
+  // },
 
-export default nextConfig;
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https', // ✅ HTTP/HTTPS 명확하게 설정 가능
+        hostname: 'www.course-api.com', // ✅ 특정 호스트만 허용(보안 우수)
+        port: '', // ✅ 특정 포트만 허용 가능 (일반적으로 생략)
+        pathname: '/images/**' // ✅ 특정 경로 패턴만 허용 가능
+      }
+    ]
+  }
+}
+
+export default nextConfig
