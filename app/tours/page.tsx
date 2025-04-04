a473403e05aea4ab2e248117d27f3fe1584af5b4
yuna <yuna744@gmail.com>
2025-04-04 19:42:40 +0900

    Responsive Images

--- app/tours/page.tsx
+++ app/tours/page.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import Link from 'next/link'
 
 const url = 'https://www.course-api.com/react-tours-project'
@@ -25,13 +26,18 @@ async function ToursPage() {
     <section>
       <h1 className="text-3xl mb-4">Tours</h1>
 
-      {data.map((tour) => {
-        return (
-          <Link href={`/tours/${tour.id} `} key={tour.id} className="hover:text-blue-500">
-            <h2>{tour.name}</h2>
-          </Link>
-        )
-      })}
+      <div className="grid md:grid-cols-2 gap-8">
+        {data.map((tour) => {
+          return (
+            <Link key={tour.id} href={`/tours/${tour.id}`} className="hover:text-blue-500">
+              <div className="relative h-48 mb-2">
+                <Image src={tour.image} alt={tour.name} fill /*이미지가 부모 요소의 크기에 맞춰 꽉 채워짐, 부모 relative 필수*/ sizes="100vw" priority/*우선적으로 로드*/ className="object-cover rounded" />
+              </div>
+              <h2>{tour.name}</h2>
+            </Link>
+          )
+        })}
+      </div>
     </section>
   )
 }
