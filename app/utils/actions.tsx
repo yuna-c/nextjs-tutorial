c20806cb8f05418418f212ce89be48b51c83f56a
yuna <yuna744@gmail.com>
2025-04-07 20:44:17 +0900

    Save User -서버 작업 내의 백엔드 처리(json)

--- app/utils/actions.tsx
+++ app/utils/actions.tsx
@@ -1,10 +1,30 @@
 'use server'
+import { readFile, writeFile } from 'fs/promises'
+
+type User = {
+  id: string
+  firstName: string
+  lastName: string
+}
 
 export const createUser = async (formData: FormData) => {
   'use server'
   const firstName = formData.get('firstName') as string
   const lastName = formData.get('lastName') as string
-  const rawData = Object.fromEntries(formData)
-  console.log(rawData)
-  console.log({ firstName, lastName })
+  const newUser: User = { firstName, lastName, id: Date.now().toString() }
+
+  // console.log({ firstName, lastName })
+  await saveUser(newUser)
+}
+
+export const fetchUsers = async (): Promise<User[]> => {
+  const result = await readFile('users.json', { encoding: 'utf8' })
+  const users = result ? JSON.parse(result) : []
+  return users
+}
+
+const saveUser = async (user: User) => {
+  const users = await fetchUsers()
+  users.push(user)
+  await writeFile('users.json', JSON.stringify(users))
 }
