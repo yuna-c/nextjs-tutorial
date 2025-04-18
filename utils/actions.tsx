0d0c9d7089c21477c56a4dd7f8fc3f9eba0bf14a
yuna <yuna744@gmail.com>
2025-04-18 23:06:53 +0900

    Route Handlers - POST - POST 요청으로 사용자 등록 기능 구현 - /api/users에 사용자 생성용 POST 핸들러 추가

--- utils/actions.tsx
+++ utils/actions.tsx
@@ -44,7 +44,7 @@ export const fetchUsers = async (): Promise<User[]> => {
   return users
 }
 
-const saveUser = async (user: User) => {
+export const saveUser = async (user: User) => {
   const users = await fetchUsers()
   users.push(user)
   await writeFile('users.json', JSON.stringify(users))
