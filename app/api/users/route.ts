bfa55f55ded21c6a6fb2c69001a86a3c0dd14f28
yuna <yuna744@gmail.com>
2025-04-18 21:14:38 +0900

    Route Handlers - Params - /api/users GET 핸들러 및 id 쿼리 파라미터 파싱 추가 - Node.js 기반에서 Request 정보를 래핑해서 전달

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,6 +1,11 @@
 import { fetchUsers, saveUser } from '@/utils/actions'
 
-export const GET = async () => {
+export const GET = async (req: Request) => {
+  console.log(req)
+  const { searchParams } = new URL(req.url)
+  const id = searchParams.get('id')
+  console.log(id)
+
   const users = await fetchUsers()
   // return Response.json({ msg: 'api route' })
   return Response.json({ users })
