5e342ec473a9bcbb4d9782ee619ce35d4d975ec5
yuna <yuna744@gmail.com>
2025-04-18 23:37:06 +0900

    Middleware - /about 및 /tours 경로 접근 시 루트로 리다이렉트하는 미들웨어 추가 - /about/*, /tours/* 경로를 /로 리디렉션 처리

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,11 @@
-export function middleware() {
-  // console.log('hello crise')
-  return Response.json({ msg: 'hello crise' })
+import { NextResponse } from 'next/server'
+
+export function middleware(request: Request) {
+  console.log('hello crise')
+  // return Response.json({ msg: 'hello crise' })
+  return NextResponse.redirect(new URL('/', request.url))
 }
 
 export const config = {
-  matcher: '/about'
+  matcher: ['/about/:path*', '/tours/:path*']
 }
