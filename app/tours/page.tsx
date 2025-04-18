9af46652902c34b0027c8da9677d21a14f26f4fa
yuna <yuna744@gmail.com>
2025-04-19 01:25:18 +0900

    Cache - 한 번 계산한 결과(HTML, API 응답 등)를 다시 사용하기 위해 저장 - 속도 향상/ 서버 부하 감소/ 비용  절감/ 확장성/ 일관성 유지 - 정적 페이지, 자주 안 바뀌는 데이터, 리스트형 페이지 등은 반드시 캐싱 - 동적 데이터라도 ISR, 헤더 캐시, SWR 등으로 전략적으로 캐싱

--- app/tours/page.tsx
+++ app/tours/page.tsx
@@ -12,6 +12,9 @@ type Tour = {
 }
 
 const fetchTours = async () => {
+  console.log('######################')
+  console.log('Fetching Tours!!')
+  console.log('######################')
   await new Promise((resolve) => setTimeout(resolve, 3000))
   const response = await fetch(url)
   const data: Tour[] = await response.json()
