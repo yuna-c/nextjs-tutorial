b9c544683d2e6f0dcda593115b68de33c65c591b
yuna <yuna744@gmail.com>
2025-04-18 21:20:35 +0900

    GET 핸들러에 NextRequest/NextResponse 적용하여 쿼리 및 리다이렉트 개선 - Next.js 전용 확장 객체인 NextRequest, NextResponse - searchParams, cookies, nextUrl 등의 편의 기능이 추가 - 리다이렉트/쿠키/캐시/쿼리 조작

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,12 +1,10 @@
-import { fetchUsers, saveUser } from '@/utils/actions'
+// import { fetchUsers, saveUser } from '@/utils/actions'
+import { NextRequest, NextResponse } from 'next/server'
 
-export const GET = async (req: Request) => {
-  console.log(req)
-  const { searchParams } = new URL(req.url)
-  const id = searchParams.get('id')
-  console.log(id)
+export const GET = async (req: NextRequest) => {
+  console.log(req.url)
+  console.log(req.nextUrl.searchParams.get('id'))
 
-  const users = await fetchUsers()
-  // return Response.json({ msg: 'api route' })
-  return Response.json({ users })
+  // const users = await fetchUsers()
+  return NextResponse.redirect(new URL('/', req.url))
 }
